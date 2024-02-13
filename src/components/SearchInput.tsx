29b9ecc0abd8ee3bc87a44fd9ca8f95acc97a054
Fabi0o <marfab32@gmail.com>
2024-02-13 19:19:01 +0100

    fix: code refactor

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -30,11 +30,13 @@ export default function SearchInput({ setCurrentCompanyData }: Props) {
     fetch(`https://wl-api.mf.gov.pl/api/search/nip/${nip}?date=2024-02-02`)
       .then((res) => res.json())
       .then(({ result }: { result: { subject: fetchResult } }) => {
+        const { subject } = result;
+
         setCurrentCompanyData({
-          name: result.subject.name,
-          NIP: Number(result.subject.nip),
-          address: result.subject.residenceAddress,
-          accountNumbers: result.subject.accountNumbers,
+          name: subject.name,
+          NIP: Number(subject.nip),
+          address: subject.residenceAddress,
+          accountNumbers: subject.accountNumbers,
         });
       });
   };
